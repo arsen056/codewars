4ae8eca0ce0324cdb2880291bf9bfa43d297fb15
arsen056 <vurdihanov@gmail.com>
2023-05-06 10:24:56 +0300

    add task

---
+++ 6kyu/Count the smiley faces.js
@@ -0,0 +1,27 @@
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+        const smile = arr[i]
+
+        if (isSmile(smile)) count++
+    }
+
+    return count;
+}
+
+function isSmile(smile) {
+    if ([':',';'].includes(smile[0])) {
+        if (['-', '~'].includes(smile[1])) {
+            return [')', 'D'].includes(smile[2]);
+            
+        }
+
+        return [')', 'D'].includes(smile[1]);
+
+    }
+    return false
+}
+
+console.log(countSmileys([':D',':~)',';~D',':)'])) // 4
+console.log(countSmileys([':)',':(',':D',':O',':;'])) // 2
